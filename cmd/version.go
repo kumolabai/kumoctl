8232bfbc28ce9ed338e9b4b1311207b87e7fc6c8
Esteban Garcia <estu91@gmail.com>
2025-10-01 15:06:20 +0100

    fix: Add github workflow test and release

--- cmd/version.go
+++ cmd/version.go
@@ -2,8 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 var versionCmd = &cobra.Command{
@@ -11,7 +18,11 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of kumoctl",
 	Long:  `All software has versions. This is kumoctl's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kumoctl v0.0.1")
+		fmt.Printf("kumoctl %s\n", version)
+		fmt.Printf("  Commit: %s\n", commit)
+		fmt.Printf("  Built:  %s\n", date)
+		fmt.Printf("  OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+		fmt.Printf("  Go:     %s\n", runtime.Version())
 	},
 }
 
